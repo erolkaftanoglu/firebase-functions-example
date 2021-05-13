b383746d89e2f4378bfacd2dfcd26a7427788543
Erol <erolkaftanoglu@gmail.com>
2021-05-14 01:18:25 +0300

    Update

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require("firebase-functions");
+let Parser = require('rss-parser');
+let parser = new Parser();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+exports.helloWorld = functions.https.onRequest(async (request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    let feed = await parser.parseURL('https://news.ycombinator.com/rss');
+    response.send({
+        feed: feed.items
+    });
 });
